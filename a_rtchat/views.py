18b73f9c2e981da07727d3243941ec815b114a88
Amol Shinde <shindeamol410@gmail.com>
2024-06-22 09:24:12 +0530

    solved issues index

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -7,5 +7,5 @@ from django.contrib.auth.decorators import login_required
 def chat_view(request):
     chat_group = get_object_or_404(ChatGroup, group_name='public-chat')
     chat_messages = chat_group.chat_messages.all()[:30]
-    return render(request, 'a_rtchat/chat.html', {'chat_messages': chat_messages})
+    return render(request, 'a_rtchat/index.html', {'chat_messages': chat_messages})
 
